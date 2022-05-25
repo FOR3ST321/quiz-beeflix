1aa5a86bfecdf9e0310a696a6627086750e0289e
FOR3ST321 <mkelvin3321@gmail.com>
2022-05-25 17:07:49 +0700

    home + category

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Genre;
 use App\Models\Movie;
+use App\Models\Episode;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller as BaseController;
 
@@ -10,19 +12,22 @@ class Controller extends BaseController
 {
     public function home(){
         return view('home', [
+            'genre' => Genre::all(),
             'movie' => Movie::orderBy('genre_id', 'ASC')->get()
         ]);
     }
 
     public function movieDetail(Request $request){
         return view('movieDetail', [
-            'movie' => Movie::where('id', $request->id)->get()
+            'movie' => Movie::where('id', $request->id)->get()->first(),
+            'episodes' => Episode::where('movie_id', $request->id)->paginate(3)
         ]);
     }
 
     public function movieCategory(Request $request){
         // dump($request->id);
         return view('categories', [
+            'category' => Genre::where('id', $request->id)->get()->first(),
             'movie' => Movie::where('genre_id', $request->id)->get()
         ]);
     }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,3 +1,59 @@
-@foreach ($movie as $item)
-    <?=dump($item);?>
-@endforeach
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
+        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.1.1/css/fontawesome.min.css">
+    <title>Beeflix</title>
+</head>
+
+<body>
+    <div class="container-fluid" style="margin-top: 2vh">
+        <h4>BeeFlix</h4>
+
+        @foreach ($genre as $g)
+            <div class="card" style="margin-top:5vh">
+                <div class="card-header">
+                    <h5 class="card-title">
+                        <?= $g->name ?>
+                    </h5>
+                </div>
+                <div class="card-body">
+                    <div class="row">
+                        @foreach ($movie as $m)
+                            @if ($m->genre_id == $g->id)
+                                <div class="col-3">
+                                    <div class="d-flex flex-column ">
+                                        <img src="<?= $m->photo ?>" class="img-fluid" alt="Responsive image">
+                                        <p class="text-center"><?= $m->title ?></p>
+                                        <a href="./movie/<?= $m->id ?>" class="btn text-center"
+                                            style="background-color: #423d3d;color:white">
+                                            LIHAT FILM
+                                        </a>
+                                    </div>
+                                </div>
+                            @endif
+                        @endforeach
+                    </div>
+                </div>
+            </div>
+        @endforeach
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+        integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js"
+        integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"
+        integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
